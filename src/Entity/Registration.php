aa255bc9f744054a6c42b14af6cb62bb29ed72db
Tanmay Yadav <ytanmay122005@gmail.com>
2025-12-21 11:00:00 +0530

    feat: Implement Registration entity class

---
+++ src/Entity/Registration.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Drupal\event_registration\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the Registration entity.
+ *
+ * @ingroup event_registration
+ *
+ * @ContentEntityType(
+ *   id = "event_registration",
+ *   label = @Translation("Event Registration"),
+ *   base_table = "event_registration",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "owner" = "uid",
+ *   },
+ * )
+ */
+class Registration extends ContentEntityBase implements RegistrationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedTime()
+    {
+        return $this->get('created')->value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatedTime($timestamp)
+    {
+        $this->set('created', $timestamp);
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmail()
+    {
+        return $this->get('email')->value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEmail($email)
+    {
+        $this->set('email', $email);
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwner()
+    {
+        return $this->get('uid')->entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwnerId()
+    {
+        return $this->get('uid')->target_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwnerId($uid)
+    {
+        $this->set('uid', $uid);
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwner(UserInterface $account)
+    {
+        $this->set('uid', $account->id());
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function baseFieldDefinitions(EntityTypeInterface $entity_type)
+    {
+        $fields = parent::baseFieldDefinitions($entity_type);
+
+        $fields['email'] = BaseFieldDefinition::create('email')
+            ->setLabel(t('Email'))
+            ->setDescription(t('The email of the registrant.'))
+            ->setSettings([
+                'max_length' => 254,
+                'text_processing' => 0,
+            ])
+            ->setDefaultValue('')
+            ->setDisplayOptions('view', [
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => -4,
+            ])
+            ->setDisplayOptions('form', [
+                'type' => 'email_default',
+                'weight' => -4,
+            ])
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['created'] = BaseFieldDefinition::create('created')
+            ->setLabel(t('Created'))
+            ->setDescription(t('The time that the entity was created.'));
+
+        $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+            ->setLabel(t('Authored by'))
+            ->setDescription(t('The user ID of author of the registration entity.'))
+            ->setSetting('target_type', 'user')
+            ->setSetting('handler', 'default')
+            ->setDisplayOptions('view', [
+                'label' => 'hidden',
+                'type' => 'author',
+                'weight' => 0,
+            ])
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        return $fields;
+    }
+
+}
