0ff25531b14819b3804f1216ef6ec0caaee06a40
Tanmay Yadav <ytanmay122005@gmail.com>
2025-12-28 10:00:00 +0530

    test: Implement Registration entity kernel tests

--- tests/src/Kernel/RegistrationEntityTest.php
+++ tests/src/Kernel/RegistrationEntityTest.php
@@ -15,7 +15,7 @@ class RegistrationEntityTest extends KernelTestBase
     /**
      * {@inheritdoc}
      */
-    protected static $modules = ['event_registration', 'user', 'system'];
+    protected static $modules = ['event_registration', 'user', 'system', 'node', 'field', 'text'];
 
     /**
      * {@inheritdoc}
@@ -25,6 +25,20 @@ class RegistrationEntityTest extends KernelTestBase
         parent::setUp();
         $this->installEntitySchema('event_registration');
         $this->installEntitySchema('user');
+        $this->installConfig(['event_registration']);
+    }
+
+    public function testEntity()
+    {
+        $user = $this->createUser();
+        $registration = \Drupal\event_registration\Entity\Registration::create([
+            'uid' => $user->id(),
+            'email' => 'test@example.com',
+        ]);
+        $registration->save();
+
+        $this->assertEquals($user->id(), $registration->getOwnerId());
+        $this->assertEquals('test@example.com', $registration->getEmail());
     }
 
     /**
